d0c42042793b7d5d3821c8c6aa589723479d0417
Srdjan S <stankovic.srdjo@gmail.com>
2019-12-28 18:49:24 +0100

    Clean doc content on doc creation

--- src/comparedocs/document.py
+++ src/comparedocs/document.py
@@ -1,3 +1,4 @@
+import string
 import textract
 from pathlib import Path
 
@@ -5,7 +6,7 @@ from pathlib import Path
 class Document:
 
     # TODO: Add Enum types for different document types
-    __slots__ = {'name', 'path', 'type', 'content'}
+    __slots__ = {'name', 'path', 'type', '_dirty_content', '_content', 'embedded_content'}
 
     def __init__(self, path: str) -> None:
         path = Path(path)
@@ -14,4 +15,31 @@ class Document:
         self.name = path.stem
         self.type = path.suffix
         self.content = textract.process(path).decode('utf-8')
+
+    def embed_words(self):
+        pass
+
+    def clean_text(self, text: str) -> str:
+        """
+        Cleans text from any punctuation characters and lowers it.
+        Args:
+            text: Text to be cleaned.
+
+        Returns: Lowered string wihout punctuation sings.
+        """
+        return text.lower().translate(str.maketrans('', '', string.punctuation))
+
+    @property
+    def content(self):
+        """
+        Returns original document content.
+        """
+        return self._dirty_content
+
+    @content.setter
+    def content(self, val: str):
+        # cleans text before assigning it to _content
+        self._content = self.clean_text(val)
+        # assigns original text to `dirty_content` attribute
+        self._dirty_content = val
 
